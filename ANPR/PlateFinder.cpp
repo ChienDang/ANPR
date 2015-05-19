4f325277f6bcdce3bfd2295ce58344d5c56c6739
Chien Proger <chienddbk@gmail.com>
2015-05-19 15:29:46 +0700

    Bắt đầu tiền xử lý ảnh

--- ANPR/PlateFinder.cpp
+++ ANPR/PlateFinder.cpp
@@ -3,9 +3,23 @@
 
 PlateFinder::PlateFinder()
 {
+	S1 = cvCreateStructuringElementEx(3, 1, 1, 0, CV_SHAPE_RECT, NULL);
+	S2 = cvCreateStructuringElementEx(6, 1, 3, 0, CV_SHAPE_RECT, NULL);
+	plate = NULL;
 }
 
 
 PlateFinder::~PlateFinder()
 {
+	if (plate)
+	{
+		S1 = NULL;
+		S2 = NULL;
+		plate = NULL;
+	}
+}
+
+void PlateFinder::ImageRestoration(IplImage *src)
+{
+
 }
--- ANPR/PlateFinder.h
+++ ANPR/PlateFinder.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <opencv\cv.h>
 #include <opencv\highgui.h>
@@ -6,8 +6,13 @@
 #include <iostream>
 class PlateFinder
 {
+private:
+	IplConvKernel* S1;
+	IplConvKernel* S2;
+	IplImage *plate;
 public:
 	PlateFinder();
 	virtual ~PlateFinder();
+	void ImageRestoration(IplImage *src); // Hàm tiền xử lý ảnh gốc
 };
 
